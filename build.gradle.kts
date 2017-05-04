f0a8df7630ad61420873dc10faf0f0e74fdf2796
Lloyd Ramey <lnr0626@gmail.com>
2017-05-04 18:50:18 -0400

    Configure encoding

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
+import org.gradle.api.JavaVersion
+import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.script.lang.kotlin.dependencies
+import org.gradle.script.lang.kotlin.eclipse
+import org.gradle.script.lang.kotlin.idea
+import org.gradle.script.lang.kotlin.java
+import org.gradle.script.lang.kotlin.maven
+import org.gradle.script.lang.kotlin.repositories
+import org.gradle.script.lang.kotlin.testCompile
+
 plugins {
     java
     maven
@@ -13,6 +23,10 @@ description = """JDBC Named Parameters"""
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType(JavaCompile::class.java) {
+    options.encoding = "UTF-8"
 }
 
 repositories {

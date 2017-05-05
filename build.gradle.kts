22052864f6c39a4dfa68b1c5e6e05d4617a54ac3
Lloyd Ramey <lnr0626@gmail.com>
2017-05-04 21:40:40 -0400

    Setup publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,40 @@
 
+import org.gradle.script.lang.kotlin.`java-library`
 import org.gradle.script.lang.kotlin.`maven-publish`
 import org.gradle.script.lang.kotlin.dependencies
 import org.gradle.script.lang.kotlin.eclipse
 import org.gradle.script.lang.kotlin.idea
 import org.gradle.script.lang.kotlin.java
 import org.gradle.script.lang.kotlin.maven
-import org.gradle.script.lang.kotlin.publishing
 import org.gradle.script.lang.kotlin.repositories
 import org.gradle.script.lang.kotlin.testCompile
 import org.gradle.script.lang.kotlin.version
+buildscript {
+    repositories {
+        jcenter()
+    }
+
+    dependencies {
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.9.13")
+    }
+}
 
 plugins {
     java
     maven
     idea
     eclipse
-    publishing
+    `java-library`
     `maven-publish`
     id("nebula.kotlin") version("1.1.2")
     id("nebula.release") version("4.2.0")
-    id("com.jfrog.bintray") version("1.7")
+    id("com.jfrog.bintray") version("1.7.3")
 }
 
-apply { from("bintray.gradle") }
+apply {
+    plugin("org.jetbrains.dokka")
+    from("bintray.gradle")
+}
 
 group = "com.lloydramey.jdbc"
 
